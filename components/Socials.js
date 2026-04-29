bfc08acbdf25b2237ceb015430113b33e0a14e61
kscodeur <kinhosamuel2@gmail.com>
2026-04-29 23:44:36 +0000

    fix: lien github et linkedin pour s'ouvrir dans un nouvel onglet

--- components/Socials.js
+++ components/Socials.js
@@ -2,10 +2,10 @@ import Link from "next/link";
 import { RiLinkedinLine, RiGithubLine } from 'react-icons/ri';
 const Socials = () => {
   return <div className="flex items-center gap-x-5 text-lg">
-    <Link href={'https://www.linkedin.com/in/samuelkinho'} className="hover:text-accent transition-all duration-300">
+    <Link href={'https://www.linkedin.com/in/samuelkinho'} target="_blank" className="hover:text-accent transition-all duration-300">
       <RiLinkedinLine/>
     </Link>
-    <Link href={'https://github.com/KScodeur'} className="hover:text-accent transition-all duration-300">
+    <Link href={'https://github.com/KScodeur'} target="_blank" className="hover:text-accent transition-all duration-300">
       <RiGithubLine/>
     </Link>
   </div>;
